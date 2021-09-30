ec17f449ed59c705deb4e2a503e8e00b159e0592
Zeuzith <marcelsworth@gmail.com>
2021-09-30 16:32:17 -0400

    Got a new owner page creation working

---
+++ pythonlogin/config.py
@@ -0,0 +1,4 @@
+mysql_host ='finalproject.cv5rjrwx9nu4.us-east-1.rds.amazonaws.com'
+mysql_user = 'admin'
+mysql_password ='adminpassword'
+mysql_db = 'finalProject'
--- pythonlogin/main.py
+++ pythonlogin/main.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import config
+
 
 app = Flask(__name__)
 
 # Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
 
+
+
 # Enter your database connection details below
-app.config['MYSQL_HOST'] = 'finalproject.cv5rjrwx9nu4.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'adminpassword'
-app.config['MYSQL_DB'] = 'finalProject'
+app.config['MYSQL_HOST'] = config.mysql_host
+app.config['MYSQL_USER'] = config.mysql_user
+app.config['MYSQL_PASSWORD'] = config.mysql_password
+app.config['MYSQL_DB'] = config.mysql_db
 
 # Intialize MySQL
 mysql = MySQL(app)
@@ -91,7 +95,7 @@ def register():
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM finalProject WHERE userName = %s', (username,))
+        cursor.execute('SELECT * FROM user WHERE userName = %s', (username,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
@@ -104,7 +108,7 @@ def register():
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO finalProject (userName, password, email) VALUES ( %s, %s, %s)',
+            cursor.execute('INSERT INTO user (userName, pass, Email) VALUES ( %s, %s, %s)',
                            (username, password, email,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
@@ -117,6 +121,49 @@ def register():
     return render_template('register.html', msg=msg)
 
 
+# http://localhost:5000/Flask/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/newOwner', methods=['GET', 'POST'])
+def newOwner():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'LastName' in request.form and 'FirstName' in request.form and 'Email' in request.form:
+        # Create variables for easy access
+        FirstName = request.form['FirstName']
+        LastName = request.form['LastName']
+        Email = request.form['Email']
+        mailingAddress = request.form['mailingAddress']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode']
+        print('Hello World')
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Owners WHERE Email = %s', (Email,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', Email):
+            msg = 'Invalid email address!'
+        # elif not re.match(r'[A-Za-z0-9]+', username):
+        #     msg = 'Username must contain only characters and numbers!'
+        elif not FirstName or not LastName or not Email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            
+            cursor.execute('INSERT INTO Owners (FirstName, LastName, Email, MailingAddress, city, state, zip) VALUES ( %s, %s, %s, %s, %s, %s, %s)',
+                           (FirstName,LastName, Email, mailingAddress, city, state, zipcode))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('newOwner.html', msg=msg)
 
 
 
